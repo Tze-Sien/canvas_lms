8ef3007ca764c207f9a726299b9a8b3c4abbaae6
jc <jiancheng8064@gmail.com>
2025-02-21 11:26:50 +0800

    link to profile page

---
+++ Pages/Profile/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CanvasLMS.Pages.Profile
+{
+
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
